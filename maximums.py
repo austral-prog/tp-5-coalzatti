cd2d7bd7025fdbf7cf7a7760a58c50d55c24de7c
Constanza Sofía Alzatti <162724190+coalzatti@users.noreply.github.com>
2024-04-19 11:00:12 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -6,9 +6,9 @@ def max_of_two(x, y):
 
 
 def max_of_three(x, y, z):
-    if x>y or x>z:
+    if x>y and x>z:
         return(x)
-    elif y > z:
+    elif y > z and y>x:
          return(y)
     else:
          return(z)
